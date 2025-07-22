e06e8d930250b5c39caa202e30d0a2330026dc1b
Naphat <Naphat.Su@bdms.co.th>
2025-07-22 14:23:27 +0700

    ADD FK Stock

---
+++ Migrations/20250722072210_AddFKStock.cs
@@ -0,0 +1,292 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace chickko.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFKStock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StockCategoryID",
+                table: "StockLog",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockLocationID",
+                table: "StockLog",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockUnitTypeID",
+                table: "StockLog",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SupplierSupplyId",
+                table: "StockLog",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockCategoryID",
+                table: "Stock",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockLocationID",
+                table: "Stock",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockUnitTypeID",
+                table: "Stock",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "StockCategory",
+                columns: table => new
+                {
+                    StockCategoryID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StockCategoryName = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockCategory", x => x.StockCategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockLocation",
+                columns: table => new
+                {
+                    StockLocationID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StockLocationName = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockLocation", x => x.StockLocationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockUnitType",
+                columns: table => new
+                {
+                    StockUnitTypeID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StockUnitTypeName = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockUnitType", x => x.StockUnitTypeID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockLog_StockCategoryID",
+                table: "StockLog",
+                column: "StockCategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockLog_StockLocationID",
+                table: "StockLog",
+                column: "StockLocationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockLog_StockUnitTypeID",
+                table: "StockLog",
+                column: "StockUnitTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockLog_SupplierSupplyId",
+                table: "StockLog",
+                column: "SupplierSupplyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stock_StockCategoryID",
+                table: "Stock",
+                column: "StockCategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stock_StockLocationID",
+                table: "Stock",
+                column: "StockLocationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stock_StockUnitTypeID",
+                table: "Stock",
+                column: "StockUnitTypeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stock_StockCategory_StockCategoryID",
+                table: "Stock",
+                column: "StockCategoryID",
+                principalTable: "StockCategory",
+                principalColumn: "StockCategoryID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stock_StockLocation_StockLocationID",
+                table: "Stock",
+                column: "StockLocationID",
+                principalTable: "StockLocation",
+                principalColumn: "StockLocationID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stock_StockUnitType_StockUnitTypeID",
+                table: "Stock",
+                column: "StockUnitTypeID",
+                principalTable: "StockUnitType",
+                principalColumn: "StockUnitTypeID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StockLog_StockCategory_StockCategoryID",
+                table: "StockLog",
+                column: "StockCategoryID",
+                principalTable: "StockCategory",
+                principalColumn: "StockCategoryID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StockLog_StockLocation_StockLocationID",
+                table: "StockLog",
+                column: "StockLocationID",
+                principalTable: "StockLocation",
+                principalColumn: "StockLocationID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StockLog_StockUnitType_StockUnitTypeID",
+                table: "StockLog",
+                column: "StockUnitTypeID",
+                principalTable: "StockUnitType",
+                principalColumn: "StockUnitTypeID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StockLog_Supplier_SupplierSupplyId",
+                table: "StockLog",
+                column: "SupplierSupplyId",
+                principalTable: "Supplier",
+                principalColumn: "SupplyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stock_StockCategory_StockCategoryID",
+                table: "Stock");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stock_StockLocation_StockLocationID",
+                table: "Stock");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stock_StockUnitType_StockUnitTypeID",
+                table: "Stock");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StockLog_StockCategory_StockCategoryID",
+                table: "StockLog");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StockLog_StockLocation_StockLocationID",
+                table: "StockLog");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StockLog_StockUnitType_StockUnitTypeID",
+                table: "StockLog");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StockLog_Supplier_SupplierSupplyId",
+                table: "StockLog");
+
+            migrationBuilder.DropTable(
+                name: "StockCategory");
+
+            migrationBuilder.DropTable(
+                name: "StockLocation");
+
+            migrationBuilder.DropTable(
+                name: "StockUnitType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StockLog_StockCategoryID",
+                table: "StockLog");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StockLog_StockLocationID",
+                table: "StockLog");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StockLog_StockUnitTypeID",
+                table: "StockLog");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StockLog_SupplierSupplyId",
+                table: "StockLog");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Stock_StockCategoryID",
+                table: "Stock");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Stock_StockLocationID",
+                table: "Stock");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Stock_StockUnitTypeID",
+                table: "Stock");
+
+            migrationBuilder.DropColumn(
+                name: "StockCategoryID",
+                table: "StockLog");
+
+            migrationBuilder.DropColumn(
+                name: "StockLocationID",
+                table: "StockLog");
+
+            migrationBuilder.DropColumn(
+                name: "StockUnitTypeID",
+                table: "StockLog");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierSupplyId",
+                table: "StockLog");
+
+            migrationBuilder.DropColumn(
+                name: "StockCategoryID",
+                table: "Stock");
+
+            migrationBuilder.DropColumn(
+                name: "StockLocationID",
+                table: "Stock");
+
+            migrationBuilder.DropColumn(
+                name: "StockUnitTypeID",
+                table: "Stock");
+        }
+    }
+}
--- Models/Stock.cs
+++ Models/Stock.cs
@@ -10,11 +10,17 @@ namespace chickko.api.Models
         [Required]
         public string ItemName { get; set; } = string.Empty; //ชื่อของสิ่งของนั้น
         [Required]
+        public int StockCategoryID { get; set; }
+        public StockCategory? StockCategory { get; set; } = null;
         public int RequiredQTY { get; set; } // จำนวนที่ต้องการ
         public int TotalQTY { get; set; } // จำนวนคงเหลือ
         public int StockInQTY { get; set; } //จำนวนที่ซื้อเพิ่ม
+        public int StockUnitTypeID { get; set; }
+        public StockUnitType? StockUnitType { get; set; }
         public DateOnly UpdateDate { get; set; } //วันที่อัพเดท วันที่แก้ไข stock ปัจจุบัน
         public TimeOnly UpdateTime { get; set; } //เวลาที่อัพเดท วันที่แก้ไข stock ปัจจุบัน
+        public int StockLocationID { get; set; }
+        public StockLocation? StockLocation { get; set; }
         public string Remark { get; set; } = string.Empty;
     }
     public class StockLog //stock log model
@@ -29,13 +35,21 @@ namespace chickko.api.Models
         public DateOnly StockInDate { get; set; } //วันที่ซื้อเข้า
         public TimeOnly StockInTime { get; set; } //เวลาซื้อเข้า
         public int RequiredQTY { get; set; } = 0!;// จำนวนที่ต้องการ
+        public int StockCategoryID { get; set; }
+        public StockCategory? StockCategory { get; set; } = null;
+
         public int TotalQTY { get; set; } = 0;// จำนวนคงเหลือ
         public int StockInQTY { get; set; } = 0;//จำนวนที่ต้องซื้อเพิ่ม
         public int PurcheseQTY { get; set; } = 0; //จำนวนที่ซื้อจริง
         public int DipQTY { get; set; } = 0;//จำนวนที่มันดิปกันอยู่
+        public int StockUnitTypeID { get; set; }
+        public StockUnitType? StockUnitType { get; set; }
         public int Price { get; set; } = 0;//ราคาที่ซื้อ
         public bool IsPurchese { get; set; } = false;
+        public int StockLocationID { get; set; }
+        public StockLocation? StockLocation { get; set; }
         public int SupplyId { get; set; } = 0;
+        public Supplier? Supplier { get; set; } = null;
         public string Remark { get; set; } = string.Empty;
 
     }
